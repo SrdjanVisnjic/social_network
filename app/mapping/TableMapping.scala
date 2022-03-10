f1f8585755d4f1d4c4b256bff09e1d7b3bfdf28b
Srdjan Visnjic <visnjic.srdjan@jjzmaj.edu.rs>
2022-03-10 13:49:29 +0100

    mapping

--- app/mapping/TableMapping.scala
+++ app/mapping/TableMapping.scala
@@ -1,7 +1,10 @@
  package mapping
+ import models.{Likes, Post, User, UserFriend}
+
  import javax.inject.Inject
  import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
  import slick.jdbc.JdbcProfile
+
  import scala.concurrent.ExecutionContext
  import slick.sql.SqlProfile.ColumnOption.SqlType
 
@@ -11,10 +14,9 @@
                             )(
                               implicit executionContext: ExecutionContext
                             ) extends HasDatabaseConfigProvider[JdbcProfile]{
-
+   import profile.api._
   private class UserTable(tag:Tag) extends Table[User](tag, "user"){
 
-    import profile.api._
 
     def id = column[Long]( "id", O.PrimaryKey, O.AutoInc)
     def username = column[String]("userName")
@@ -34,12 +36,50 @@
   val users = TableQuery[UserTable]
 
   private class PostTable(tag: Tag) extends  Table[Post](tag, "post"){
+
     def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
     def messsage = column[String]("message")
     def createdAt = column[String]("createdAt")
     def editedAt = column[String]("editedAt")
     def userId = column[Long]("userId")
+
     def userFk = foreignKey("fk_post_user", userId, users)(_.id)
+
+    def userIndex = index("idx_post_user", userId, false)
+
+    def * =(id,userId,messsage,createdAt,editedAt)<>(Post.tupled, Post.unapply)
   }
+ val posts = TableQuery[PostTable]
+
+ private class LikesTable(tag: Tag) extends Table[Likes](tag, "likes"){
+   def userId = column[Long]("userId")
+   def postId = column[Long]("postId")
+
+   def userFk = foreignKey("fk_likes_user", userId, users)(_.id)
+   def postFk = foreignKey("fk_likes_post", postId, posts)(_.id)
+
+   def userIndex = index("idx_like_user", userId, false)
+   def postIndex = index("idx_like_post",postId, false)
+
+   def * = (userId,postId)<>(Likes.tupled, Likes.unapply)
+ }
+ val likes = TableQuery[LikesTable]
+
+ private class UserFriendTable(tag: Tag) extends Table[UserFriend](tag, "userFriend"){
+   def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+   def sourceId = column[Long]("sourceId")
+   def targetId = column[Long]("targetId")
+   def createdAt = column[String]("createdAt")
+   def status = column[Int]("status")
+
+   def sourceFk = foreignKey("fk_friend_source", sourceId, users)(_.id)
+   def targetFk = foreignKey("fk_friend_target", targetId, users)(_.id)
+
+   def sourceIndex = index("idx_friend_source", sourceId, false)
+   def targetIndex = index("idx_friend_target", targetId, false)
+
+   def * = (id,sourceId,targetId,createdAt,status)<>(UserFriend.tupled, UserFriend.unapply)
+ }
+ val userfriends = TableQuery[UserFriendTable]
 }
 
